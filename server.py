7cac2479040c8fb813d78a95c46ea3cd0ad67d72
Srdjan Grubor <sgnn7@sgnn7.org>
2016-10-21 18:29:06 -0500

    Removed odd WSGI prefix variable

--- server.py
+++ server.py
@@ -9,22 +9,20 @@ from flask import redirect, url_for
 from globalhack.services import register_services
 
 
-WSGI_PATH_PREFIX = ''
-
 class Config():
-    def __init__(self,app,WSGI_PATH_PREFIX):
+    def __init__(self, app, prefix):
         self.app = app
         # register managers and then services
         print'----------------------------------------------------------------------------'
-        print '                   WSGIPrefix set to %s'%WSGI_PATH_PREFIX
-        self.srvs = register_services(app, WSGI_PATH_PREFIX)
+        print '                   WSGIPrefix set to %s' % prefix
+        self.srvs = register_services(app, prefix)
 
 # create application
 application = flask.Flask(__name__)
 application.config['CONFIG_PATH'] = os.path.join(os.path.basename(__file__), "config/")
 cfg = None
 
-@application.route(WSGI_PATH_PREFIX + '/globalhack')
+@application.route('/globalhack')
 def index():
     print 'Index entry'
     return "<b>hello</b>"
@@ -32,16 +30,15 @@ def index():
 def set_wsgi_prefix(prefix='/'):
     print 'setWsgi entry'
     WSGI_PATH_PREFIX = prefix
-    cfg = Config(application, WSGI_PATH_PREFIX)
+    cfg = Config(application)
 
-@application.route(WSGI_PATH_PREFIX + '/')
+@application.route('/')
 def root():
     print 'Root entry'
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    WSGI_PATH_PREFIX = '/globalhack'
-    cfg = Config(application, WSGI_PATH_PREFIX)
+    cfg = Config(application, '/globalhack')
     dport = int(sys.argv[1]) if len(sys.argv) > 1 else 5050
 
     application.run(host='0.0.0.0',
@@ -50,5 +47,3 @@ if __name__ == '__main__':
                     use_reloader=True,
                     processes=100,
                     static_files={'/':'static'})
-else:
-    cfg = Config(application, WSGI_PATH_PREFIX)
