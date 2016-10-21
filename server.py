d29eed15a1bad5daa8a5fb0fff26302098aaaf79
Srdjan Grubor <sgnn7@sgnn7.org>
2016-10-21 18:32:19 -0500

    Cleaned up server launcher a bit more

--- server.py
+++ server.py
@@ -20,17 +20,11 @@ class Config():
 # create application
 application = flask.Flask(__name__)
 application.config['CONFIG_PATH'] = os.path.join(os.path.basename(__file__), "config/")
-cfg = None
 
 @application.route('/globalhack')
 def index():
     print 'Index entry'
     return "<b>hello</b>"
-
-def set_wsgi_prefix(prefix='/'):
-    print 'setWsgi entry'
-    WSGI_PATH_PREFIX = prefix
-    cfg = Config(application)
 
 @application.route('/')
 def root():
@@ -38,7 +32,7 @@ def root():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    cfg = Config(application, '/globalhack')
+    Config(application, '/globalhack')
     dport = int(sys.argv[1]) if len(sys.argv) > 1 else 5050
 
     application.run(host='0.0.0.0',
