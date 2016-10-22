fdd73127f47fb5f5a3a03541130a9971a0e236b1
Srdjan Grubor <sgnn7@sgnn7.org>
2016-10-22 18:36:18 -0500

    Cleaned up texting code

--- globalhack/notifier.py
+++ globalhack/notifier.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python2.7
+
 from twilio.rest import TwilioRestClient
 
-def sendMessage(_newJobs):
+def sendMessage(message, phone_number):
     client = TwilioRestClient(account="AC65f8bda5168de34947f482a09c0f2b98",
                               token="dd32bce6497b19f9f52baf3677fa8192")
-    _str = "Insert Message here"
-    #client.messages.create(from_="+13143251480",
-     #                      to="+13145831629",
-      #                     body=_str)
     client.messages.create(from_="+12198416729",
                            to="+12198412242",
-                           body=_str)
+                           body=message)
+
+if __name__ == '__main__':
+    sendMessage("Testing message", "1234567890")
