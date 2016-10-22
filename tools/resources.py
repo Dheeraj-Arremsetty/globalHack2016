0a00df643051d4c0922dbfcb1ab9f7cf330ff031
Eileen Schweiss <enschweiss@gmail.com>
2016-10-22 15:24:23 -0500

    adding reference documents for housing

---
+++ tools/resources.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+
+import pymongo
+import csv
+
+# Connection to Mongo DB
+conn = pymongo.MongoClient()
+db = conn.resources
+
+
+donationquestions = {}
+documents = ['DonationQuestions', 'ghresource', 'UtilityAssistance']
+
+reader = csv.DictReader(open('/Users/eileenschweiss/Desktop/Resources/'+ 'ghresource' + '.csv'), delimiter=',')
+for row in reader:
+    print row
+    db['ghresource'].insert(row)
+
+
+
+
