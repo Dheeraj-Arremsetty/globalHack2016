9d606213da652a938847bc6fd154cb40d97ab1d5
Eric Ray <erichamion@gmail.com>
2016-10-22 05:10:26 -0500

    troubleshooting providerController

--- static/scripts/local/controllers/provider.js
+++ static/scripts/local/controllers/provider.js
@@ -3,10 +3,6 @@
 // <script src="/scripts/local/app.js" />
 
 app.controller('providerController', function($scope) {
-    $scope.$on('$locationChangeStart', function(event) {
-        $scope.updateProviderInfo();
-    });
-    
     $scope.updateProviderInfo = function() {
         alert('Fetching provider info');
         $scope.get('/provider')
