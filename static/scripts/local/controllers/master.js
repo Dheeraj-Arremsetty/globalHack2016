d57c48708b9e525a1b549287ca44d763aaa32bdd
Eric Ray <erichamion@gmail.com>
2016-10-22 05:42:12 -0500

    troubleshooting providerController

--- static/scripts/local/controllers/master.js
+++ static/scripts/local/controllers/master.js
@@ -8,16 +8,40 @@ app.controller('master', function($scope, $http, $localStorage, $q) {
     };
     
     $scope.post = function(url, data) {
-        return $scope.$storage.token ? httpPostWithAuth(url, data) : $http.post(url, data);
+        return $scope.$storage.token ? $http.post(url, data, getAuthConfig()) : $http.post(url, data);
     }
     
     function httpGetWithAuth(url) {
-        return httpRequestWithAuth('get', url);
+        return $http.get(url, getAuthConfig())
+            .catch(function(resp) {
+                if (resp.status === 401) {
+                    // Not authenticated, or authentication expired. Token is not valid.
+
+                    alert('Login session has expired. Please enter your credentials.');
+
+                    // Kill the stored token.
+                    delete $scope.$storage.token;
+
+                    // Return an empty object just to appease anything that might be waiting on
+                    // the results of this call.
+                    return {};
+                } else {
+                    // Request failed for some reason other than lack of authentication. 
+                    // Pass the failure on.
+                    return $q.reject(resp);
+                }
+        });
     }
     
-    function httpPostWithAuth(url, data) {
-        return httpRequestWithAuth('post', url, data);
+    
+    function getAuthConfig() {
+        return {
+            headers: {
+                Authorization: 'Bearer ' + $scope.$storage.token
+            }
+        };
     }
+
     
     function httpRequestWithAuth(method, url, data) {
         return $http[method](url, data)
