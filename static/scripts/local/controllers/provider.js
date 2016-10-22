d6b5ef9e99f4da4f228e843344067a5023028423
Eric Ray <erichamion@gmail.com>
2016-10-22 04:11:33 -0500

    Added basic provider info handling

--- static/scripts/local/controllers/provider.js
+++ static/scripts/local/controllers/provider.js
@@ -2,7 +2,19 @@
 // Must include app.js, ex:
 // <script src="/scripts/local/app.js" />
 
-app.controller('providerController', function($scope, $http) {
-    $scope.message = "Hello";
+app.controller('providerController', function($scope) {
+    $scope.$on('$locationChangeStart', function(event) {
+        $scope.updateProviderInfo();
+    });
+    
+    $scope.updateProviderInfo = function() {
+        $scope.get('/provider')
+            .then(function(resp) {
+                $scope.providerInfo = resp.data;
+            })
+            .catch(function(resp) {
+                $scope.providerInfo = { Error: 'There was a problem retrieving the information.' };
+        })
+    };
  
 });
