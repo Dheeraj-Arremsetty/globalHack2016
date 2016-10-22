adc3c311ff09db4c46e35a1edcfad9c3b1084bbd
Eric Ray <erichamion@gmail.com>
2016-10-22 07:57:40 -0500

    dummy data for Provider page

--- static/scripts/local/controllers/provider.js
+++ static/scripts/local/controllers/provider.js
@@ -6,7 +6,22 @@ app.controller('providerController', function($scope) {
     $scope.updateProviderInfo = function() {
         $scope.get('/provider?provider_id=' + $scope.$storage.token)
             .then(function(resp) {
-                $scope.providerInfo = resp.data[0];
+                // $scope.providerInfo = resp.data[0];
+                $scope.providerInfo = {
+                    name: 'John Smith',
+                    street_address: '123 Main St',
+                    city: 'nowhere',
+                    state: 'MM',
+                    zip_code: '99999',
+                    description: 'Lorem ipsom dolor sic amet'
+                    needs: {
+                        food: {
+                            type: 'stuff',
+                            number_of_meals: '3',
+                            expiration: '2016-10-25'
+                        }
+                    }
+                }
             })
             .catch(function(resp) {
                 $scope.providerInfo = { Error: 'There was a problem retrieving the information.' };
@@ -14,10 +29,15 @@ app.controller('providerController', function($scope) {
     };
     
         $scope.getAllNeeds = function() {
-        $scope.get('/needs')
-            .then(function(resp) {
-                $scope.allNeeds = resp.data.result;
-        });
+//            $scope.get('/needs')
+//                .then(function(resp) {
+//                    $scope.allNeeds = resp.data.result;
+//            });
+            $acope.allNeeds = {
+                food: ['type', 'number_of_meals', 'expiration'],
+                beds: ['count'],
+                training: ['topics', 'hours']
+            }
     }
     
     $scope.sendProviderInfo = function() {};
