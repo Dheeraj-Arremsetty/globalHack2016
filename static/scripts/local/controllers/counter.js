b11f7f1ef0a3196adfc22452955a9f5ff5e02961
Eric Ray <erichamion@gmail.com>
2016-10-23 00:01:18 -0500

    minor updates to usage counters

--- static/scripts/local/controllers/counter.js
+++
@@ -1,9 +0,0 @@
-// Master Angular controller
-// Must include app.js, ex:
-// <script src="/scripts/local/app.js" />
-
-//app.controller('counterController', function($scope, $http) {
-//    
-//    
-// 
-//});
